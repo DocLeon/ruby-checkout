b754aaa7bd0acf835ebcf1db091b3b85627e2d29
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2015-10-27 21:58:56 +0000

    removed rspec deprecation warnings

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -5,13 +5,13 @@ describe Checkout do
     it 'returns 0 for no items' do
       checkout = Checkout.new
       price = checkout.scan ''
-      price.should == 0
+      expect(price).to eq(0)
     end
 
     it 'returns 50 when scanning 1 A' do
       checkout = Checkout.new
       price = checkout.scan 'A'
-      price.should == 50
+      expect(price).to eq(50)
     end
   end
 end
