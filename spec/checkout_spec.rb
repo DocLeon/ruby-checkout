6674ce54a296802d632df0d4587d884bc515cc15
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2015-10-27 22:00:42 +0000

    refactor test

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
 describe Checkout do
+  before (:each) do
+    @checkout = Checkout.new
+  end
   describe '.scan' do
     it 'returns 0 for no items' do
-      checkout = Checkout.new
-      price = checkout.scan ''
+      price = @checkout.scan ''
       expect(price).to eq(0)
     end
 
     it 'returns 50 when scanning 1 A' do
-      checkout = Checkout.new
-      price = checkout.scan 'A'
+      price = @checkout.scan 'A'
       expect(price).to eq(50)
     end
   end
