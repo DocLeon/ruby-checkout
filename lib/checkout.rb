41818d5a11367df857ce49154166814a4d2adea2
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2015-10-28 09:16:33 +0000

    refactor move catalogue into separate file

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,37 +1,11 @@
-require_relative 'checkout/item_a'
-require_relative 'checkout/item_b'
-require_relative 'checkout/item_c'
-require_relative 'checkout/item_d'
-require_relative 'checkout/no_item'
-
-class Catalogue
-  def initialize
-    @catalogue = {
-      '' => NoItem.new,
-      'A' => ItemA.new,
-      'B' => ItemB.new,
-      'C' => ItemC.new,
-      'D' => ItemD.new
-    }
-  end
-
-  def [] sku
-    @catalogue[sku]
-  end
-end
+require_relative 'checkout/catalogue'
 
 class Checkout
   def initialize (catalogue = Catalogue.new)
     @catalogue = Catalogue.new
   end
+
   def scan items
-    catalogue = {
-      '' => NoItem.new,
-      'A' => ItemA.new,
-      'B' => ItemB.new,
-      'C' => ItemC.new,
-      'D' => ItemD.new
-    }
     return @catalogue[items].price
   end
 end
---
+++ lib/checkout/catalogue.rb
@@ -0,0 +1,21 @@
+require_relative 'item_a'
+require_relative 'item_b'
+require_relative 'item_c'
+require_relative 'item_d'
+require_relative 'no_item'
+
+class Catalogue
+  def initialize
+    @catalogue = {
+      '' => NoItem.new,
+      'A' => ItemA.new,
+      'B' => ItemB.new,
+      'C' => ItemC.new,
+      'D' => ItemD.new
+    }
+  end
+
+  def [] sku
+    @catalogue[sku]
+  end
+end
