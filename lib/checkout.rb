a6acb09ea62330c8dd38634b139963483e94f1a0
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2015-10-28 09:11:36 +0000

    refactor - introduce catalogue to look after inventory

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -4,7 +4,26 @@ require_relative 'checkout/item_c'
 require_relative 'checkout/item_d'
 require_relative 'checkout/no_item'
 
+class Catalogue
+  def initialize
+    @catalogue = {
+      '' => NoItem.new,
+      'A' => ItemA.new,
+      'B' => ItemB.new,
+      'C' => ItemC.new,
+      'D' => ItemD.new
+    }
+  end
+
+  def [] sku
+    @catalogue[sku]
+  end
+end
+
 class Checkout
+  def initialize (catalogue = Catalogue.new)
+    @catalogue = Catalogue.new
+  end
   def scan items
     catalogue = {
       '' => NoItem.new,
@@ -13,6 +32,6 @@ class Checkout
       'C' => ItemC.new,
       'D' => ItemD.new
     }
-    return catalogue[items].price
+    return @catalogue[items].price
   end
 end
